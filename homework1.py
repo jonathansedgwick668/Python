81058a3fdbd0a039c822fe24ee04efea186f7608
jonathansedgwick668 <jonathansedgwick668@gmail.com>
2025-01-22 08:42:27 -0600

    Added the link to the file

--- homework1.py
+++ homework1.py
@@ -1,7 +1,7 @@
 '''
 Homework1
 Name: Jonathan Sedgwick
-github link: 
+github link: https://github.com/jonathansedgwick668/Python/edit/main/homework1.py
 Note: Remember to use comments for each function.
 doc strings should include what each input consists of, 
 what the expected output is and a description of the function.
